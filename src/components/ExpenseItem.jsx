808e3e55522f7487db95173aef6055a046ee1500
Christer <christerforsgren91@gmail.com>
2021-06-01 18:56:04 +0200

    Refactor the implementation

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,12 +1,16 @@
-import './ExpenseItem.css'
+import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const exspenseTitle = 'Car Insurence';
+  const expenseAmount = 294.67;
+
   return (
-    <div className='expense-item' >
-      <div  >March 28th 2021</div>
-      <div className='expense-item__description' >
-        <h2 className='expense-item h2' >Car Insurance</h2>
-        <div className='expense-item__price' >$294,75</div>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2 className="expense-item h2" >{exspenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
