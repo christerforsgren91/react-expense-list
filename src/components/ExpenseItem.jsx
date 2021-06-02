fb8e91ddfdfed0e6b02f2fb2ecc260fcd67f7a2f
Christer <christerforsgren91@gmail.com>
2021-06-02 20:54:42 +0200

    Refactor some syling

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -6,7 +6,7 @@ function ExpenseItem(props) {
     <div className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2 className="expense-item h2">{props.title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
